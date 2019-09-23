2a54c6c203f0c3c56190144a21b6a97deca28e83
Mark Mbugua <mbuguamark@gmail.com>
2019-09-23 16:02:29 +0300

    created a sub-category endpoint

--- specials/serializers.py
+++ specials/serializers.py
@@ -10,3 +10,16 @@ class CategorySerializer(serializers.ModelSerializer):
             'id',
             'category_name',
         )
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    id = serializers.CharField(allow_blank=False, allow_null=False, read_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = SubCategory
+        fields = (
+            'id',
+            'sub_category_name',
+            'category',
+        )
