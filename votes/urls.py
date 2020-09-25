6070ef8fe8b7cecafc5fa17bccd0da5632020518
Markize <mbuguamark@gmail.com>
2020-09-25 14:38:43 +0300

    created comment votes on FE

--- votes/urls.py
+++ votes/urls.py
@@ -12,7 +12,7 @@ app_name = 'votes'
 
 urlpatterns = [
     path('', VoteListAPIView.as_view(), name='list'),
-    path('item/', ItemVoteListAPIView.as_view(), name='item-vote-list'),
+    path('model/', ItemVoteListAPIView.as_view(), name='item-vote-list'),
     path('create/', VoteCreateAPIView.as_view(), name='create'),
     re_path('(?P<pk>[0-9A-Fa-f-]+)', VoteDetailAPIView.as_view(), name='thread'),
     # url(r'^(?P<id>\d+)/delete/$', vote_delete, name='delete'),
